7fab00314651ed6f3b2c2278522da8a0ab5f708c
ju4n1t0x <juanignaciosasia@gmail.com>
2025-11-17 22:32:11 -0300

    dashboard administrativo y crud de usuarios crados con filament, seeder agregado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
+
 
     /**
      * Seed the application's database.
@@ -16,10 +18,16 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        /*
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+        */
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@rhapp.com',
+            'password' => Hash::make('Ch4mpa1803*'),
         ]);
     }
 }
