5d34d44a7ffd6a9f5870cb918e07f435d1fde077
ju4n1t0x <juanignaciosasia@gmail.com>
2026-01-02 20:31:51 -0300

    modelos y migraciones agregadas

--- database/migrations/2015_04_04_000010_create_provinces_table.php
+++ database/migrations/2015_04_04_000010_create_provinces_table.php
@@ -3,13 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateProvincesTable extends Migration {
+class CreateProvincesTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('provinces', function(Blueprint $table) {
-			$table->increments('id');
-            $table->string('cod');
+		Schema::create('provinces', function (Blueprint $table) {
+			$table->id();
+			$table->string('cod');
 			$table->string('name');
 			$table->timestamps();
 		});
--- database/migrations/2015_04_04_000020_create_cities_table.php
+++ database/migrations/2015_04_04_000020_create_cities_table.php
@@ -3,18 +3,19 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateCitiesTable extends Migration {
+class CreateCitiesTable extends Migration
+{
 
 	public function up()
 	{
-		Schema::create('cities', function(Blueprint $table) {
-			$table->increments('id');
-			$table->integer('province_id')->unsigned();
-            $table->string('name');
-            $table->string('department')->nullable();
-            $table->decimal('latitude', 10, 8)->nullable();
-            $table->decimal('longitude', 11, 8)->nullable();
-            $table->timestamps();
+		Schema::create('cities', function (Blueprint $table) {
+			$table->id();
+			$table->foreignId('province_id')->unsigned();
+			$table->string('name');
+			$table->string('department')->nullable();
+			$table->decimal('latitude', 10, 8)->nullable();
+			$table->decimal('longitude', 11, 8)->nullable();
+			$table->timestamps();
 		});
 	}
 
